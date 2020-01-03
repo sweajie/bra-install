bcaec6687f5be131d6b84678d28585544284e180
Administrator <1620298436@qq.com>
2020-01-03 13:54:40 +0800

    修复 测试运行 文件丢失

--- public/index.php
+++ public/index.php
@@ -1,18 +1,25 @@
 <?php
+// +----------------------------------------------------------------------
+// | 鸣鹤CMS [ New Better  ]
+// +----------------------------------------------------------------------
+// | Copyright (c) 2006~2017 http://www.mhcms.net All rights reserved.
+// +----------------------------------------------------------------------
+// | Licensed ( 您必须获取授权才能进行使用 )
+// +----------------------------------------------------------------------
+// | Author: new better <1620298436@qq.com>
+// +----------------------------------------------------------------------
 namespace bra;
 
-use bra\BraCms;
+use think\App;
 
 define("API_URL", 'http://u.mhcms.net/');
 define("MODULE_NAME", "bracms");
 define("DS", DIRECTORY_SEPARATOR);
-
 require __DIR__ . '/../vendor/autoload.php';
-$_W['develop'] = false;
+$_W['develop'] = true;
 define('SYS_PATH', __DIR__ . DIRECTORY_SEPARATOR);
-//执行HTTP应用并响应
-$bra_app = new BraCms();
-$http = new BraHttp($bra_app);
+
+$http = (new App())->http;
 $response = $http->run();
 $response->send();
 $http->end($response);
